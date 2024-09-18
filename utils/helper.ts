fc4914b4ab2aa26320c6368a4895a77513c30378
mdy <h20096@e.hiroshima-cu.ac.jp>
2024-09-19 08:01:10 +0900

    本番環境url修正

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,12 +1,7 @@
 export const getURL = (path: string = "") => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL &&
-    process.env.NEXT_PUBLIC_SITE_URL.trim() !== ""
-      ? process.env.NEXT_PUBLIC_SITE_URL
-      : process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-        process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ""
-      ? process.env.NEXT_PUBLIC_VERCEL_URL
-      : "http://localhost:3000/";
+  let url = process.env.NEXT_PUBLIC_SITE_URL
+    ? process.env.NEXT_PUBLIC_SITE_URL
+    : "http://localhost:3000/";
 
   url = url.replace(/\/+$/, "");
 
