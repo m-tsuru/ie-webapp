cc279c199a88de14623b9549f081a8af91d58440
mdy <h20096@e.hiroshima-cu.ac.jp>
2024-09-19 08:06:03 +0900

    更に修正

--- utils/auth-helper/client.ts
+++ utils/auth-helper/client.ts
@@ -1,5 +1,4 @@
 import { type Provider } from "@supabase/supabase-js";
-import { getURL } from "../helper";
 import { createClient } from "../supabase/client";
 
 export async function signInWithOAuth(provider: Provider, redirectUrl: String) {
@@ -7,7 +6,8 @@ export async function signInWithOAuth(provider: Provider, redirectUrl: String) {
   await supabase.auth.signInWithOAuth({
     provider: provider,
     options: {
-      redirectTo: getURL(`/auth/callback?next=/${redirectUrl}`),
+      // redirectTo: getURL(`/auth/callback?next=/${redirectUrl}`),
+      redirectTo: `https://ie-webapp.vercel.app/auth/callback?next=/${redirectUrl}`,
     },
   });
 }
